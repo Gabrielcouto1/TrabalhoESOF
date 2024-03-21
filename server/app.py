76faa80409ce57b09f68c75d653af2f33ea6e78d
Gabriel Couto <gabrielcouto3.14@gmail.com>
2024-03-21 17:03:04 -0300

    feat: initial server setup

---
+++ server/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+import os
+
+template_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+template_dir = os.path.join(template_dir, 'TrabalhoESOF')
+template_dir = os.path.join(template_dir, 'static')
+template_dir = os.path.join(template_dir, 'frontend')
+template_dir = os.path.join(template_dir, 'templates')
+
+app = Flask("_name_", template_folder=template_dir)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
