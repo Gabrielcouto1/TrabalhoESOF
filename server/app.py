193579c19c5e2260f61db638815c1e1991e5f0c7
AnaConti <anabarbara.conti@gmail.com>
2024-03-22 12:19:32 -0300

    feat: landing page 100% done

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ app = Flask("_name_", template_folder=template_dir)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("landing_page.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
