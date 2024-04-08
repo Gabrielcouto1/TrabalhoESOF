6df4123d7b2c4a03e89db82880f8b75240169ec7
Gabriel Couto <gabrielcouto3.14@gmail.com>
2024-04-08 14:53:56 -0300

    feat: added view_products template

--- server/app.py
+++ server/app.py
@@ -76,5 +76,20 @@ def novoTarefa():
 def kanban():
     return render_template("kanban.html")
 
+@app.route("/getProductNames")
+def getProductNames():
+    data = utils.load_data()
+    product_names = [product['productName'] for product in data['scrumProducts']]
+    
+    return jsonify(product_names)
+
+@app.route("/viewProduct")
+def viewProduct():
+    product_name = request.args.get('productName')
+    data = utils.load_data()
+
+    product = next((p for p in data['scrumProducts'] if p['productName'] == product_name), None)
+    return render_template("view_product.html", product=product)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
